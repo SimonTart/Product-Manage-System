7de0a426291a38fc9df69599f8630243982fa119
曾学兵 <zengxb94@foxmail.com>
2016-04-03 20:54:44 +0800

    login page test

---
+++ src/scripts/loginBox.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TextField from "material-ui/lib/text-field";
+import RaisedButton from "material-ui/lib/raised-button";
+import IconButton from "material-ui/lib/icon-button";
+
+
+const LoginBtnStyle = {
+	width: "220px",
+
+};
+
+const LoginBox  = React.createClass({
+	render: function(){
+		return (
+			<div>
+			    <TextField hintText="用户名" /><br/>
+			    <TextField hintText="密码" /><br/>
+			    <RaisedButton label="登录" secondary={true} style={LoginBtnStyle}/>
+			</div>
+		);
+	}
+});
+
+export default LoginBox;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 var webpack = require("webpack");
 var TransferWebpackPlugin = require("transfer-webpack-plugin");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 var nodeModulePath = path.resolve(__dirname,"node_modules");
 var outputPath = path.resolve(__dirname,"dist");
@@ -14,12 +15,13 @@ module.exports = {
 	},
 	module: {
 	  loaders: [
+
 	    {
 	      test: /\.js$/,
 	      exclude: [nodeModulePath],
 	      loader: ["babel"],
 	      query:{
-	      	presets:["es2015","react"]
+	      	presets:["react","es2015"]
 	      }
 	    }
 	  ]
@@ -32,11 +34,19 @@ module.exports = {
 		}),
 		new TransferWebpackPlugin([
 			{
-				from: "./",
-				to: "../dist"
-			},
-			path.join(__dirname,"src")
-		])
+				from: "src/views",
+				to: "../../dist/views"
+			}
+		]),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new BrowserSyncPlugin({
+			host: "localhost",
+			port: "3000",
+			server: {
+				baseDir: "dist"
+			}
+		})
 	],
 	devtool: "source-map"
 };
