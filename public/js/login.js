7ee12e4686f13cbff23569493f8423e5ece43eb9
曾学兵 <zengxb94@foxmail.com>
2016-04-16 11:49:04 +0800

    account validate

--- public/js/login.js
+++ public/js/login.js
@@ -13,32 +13,35 @@
     var login = {
         validateAccount: function(account) {
             if (/[^a-zA-Z0-9]/g.test(account)) {
-                return '账号格式不正确';
+                return '账号由大小写字母和数字组成';
             }
             var accountLen = account.length;
-            if (accountLen < 5 || account > 15) {
+            if (accountLen < 5 || accountLen > 15) {
                 return '账号长度应在5-15个字符之间';
             }
             return '';
         },
-        settTips: function(tip) {
-
+        validatePassword: function(password){
+        	var passwordLen = password.length;
+        },
+        setTips: function(id, tip) {
+        	$(id).innerText = tip;
         }
     };
 
     var accountInput = $('account');
     //输入时检查
-    $on(accountInput, 'keydown change blur keyup', function() {
-        var account = accountInput.value;
-        if (/[^a-zA-Z0-9]/g.test(account)) {
-            accountInput.value = account.replace(/[^a-zA-Z0-9]/g, '');
-        }
-    });
+    // $on(accountInput, 'keydown change blur keyup', function() {
+    //     var account = accountInput.value;
+    //     if (/[^a-zA-Z0-9]/g.test(account)) {
+    //         accountInput.value = account.replace(/[^a-zA-Z0-9]/g, '');
+    //     }
+    // });
 
     accountInput.addEventListener('blur', function() {
         var account = accountInput.value;
         var result = login.validateAccount(account);
-        login.setTips(id, result);
+        login.setTips('account-tip', result);
     });
 
 
